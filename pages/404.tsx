c4fddbca6e4eeb39e18f0a9292440e613ee36986
bhanuxw <bhanuxw@gmail.com>
2022-11-11 14:52:14 +0530

    404 page apostrophe escape

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,7 +15,7 @@ const NotFound = () => {
     return (
         <div className="not-found">
             <h1>Oooops...</h1>
-            <h2>That page can't be found.</h2>
+            <h2>That page can&apos;t be found.</h2>
             <p>Go back to <Link href={'/'}>Homepage</Link></p>
         </div>
     );
