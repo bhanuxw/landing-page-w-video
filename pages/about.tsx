28b7511adcaa839878a5bb752a032c5ba866a2a2
bhanuxw <bhanuxw@gmail.com>
2022-11-11 14:44:04 +0530

    initial

---
+++ pages/about.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde expedita, repudiandae atque commodi magni nemo ab earum ratione nihil veritatis incidunt ducimus magnam a culpa. Eum, dolore. Maiores, ipsa commodi!</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde expedita, repudiandae atque commodi magni nemo ab earum ratione nihil veritatis incidunt ducimus magnam a culpa. Eum, dolore. Maiores, ipsa commodi!</p> 
+            </div>
+        </>
+    ); 
+}
+
+export default About;
---
+++ pages/ninjas/index.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: {ninjas: data}
+    };
+}
+
+const Ninjas = ({ ninjas }) => {
+    return (
+        <div>
+            {ninjas.map((ninja) => (
+                <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+                    <h3 className={styles.single}>{ninja.name}</h3> 
+                </Link>
+            ))}
+        </div>
+    );
+}
+
+export default Ninjas;
